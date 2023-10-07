faab2d016a0bfbe585fd8c3b845717a2bc161fcc
Raghav Sharma <redclaww02@gmail.com>
2023-10-07 18:35:23 +0530

    Completed reset password functionality

--- BookHarbor Frontend/src/screens/ForgotPassword.jsx
+++ BookHarbor Frontend/src/screens/ForgotPassword.jsx
@@ -1,34 +1,30 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
-const Login = () => {
-  let navigate = useNavigate();
+const ForgotPassword = () => {
+  const navigate = useNavigate();
   const [credentials, setCredentials] = useState({
     email: "",
-    password: "",
   });
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("https://book-harbor.onrender.com/api/loginuser", {
+    const response = await fetch("https://book-harbor.onrender.com/api/forgot-password", {
       method: "POST",
       headers: {
         "Content-type": "application/json",
       },
       body: JSON.stringify({
         email: credentials.email,
-        password: credentials.password,
       }),
     });
     const json = await response.json();
+    console.log(json.success);
     if (!json.success) {
       alert("Enter Valid Credentials");
-    }
-    if (json.success) {
+    } else {
+      alert("Reset email sent to your inbox");
       navigate("/");
-      localStorage.setItem("authToken", json.authToken);
-      localStorage.setItem("userEmail", credentials.email);
     }
   };
 
@@ -39,14 +35,10 @@ const Login = () => {
   return (
     <section className="bg-gray-50 dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <Link
-          to="#"
-          className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
-        ></Link>
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              Log into Existing Account
+              Reset your password
             </h1>
             <form className="space-y-4 md:space-y-6" action="#">
               <div>
@@ -57,53 +49,24 @@ const Login = () => {
                   Your email
                 </label>
                 <input
-                  value={credentials.email}
-                  onChange={onChange}
                   type="email"
                   name="email"
                   id="email"
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="name@company.com"
                   required=""
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="password"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Password
-                </label>
-                <input
-                  value={credentials.password}
                   onChange={onChange}
-                  type="password"
-                  name="password"
-                  id="password"
-                  placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required=""
+                  value={credentials.email}
                 />
               </div>
+
               <button
                 type="submit"
                 className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 border"
                 onClick={handleSubmit}
               >
-                Login
+                reset
               </button>
-              <div>
-                <Link to="/forgotpassword"><p className="text-white">forgot your password ?</p></Link>
-              </div>
-              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                Do not have an account?{" "}
-                <Link
-                  to="/signup"
-                  className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                >
-                  Signup here
-                </Link>
-              </p>
             </form>
           </div>
         </div>
@@ -112,4 +75,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default ForgotPassword;
